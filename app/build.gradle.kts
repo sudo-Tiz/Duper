a7eea63887c45607095953767f3e6e3880c0e71a
sudo-Tiz <mathis.worksome@simplelogin.fr>
2026-06-30 17:18:34 +0200

    chore: upgrade sdk 36 -> 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 android {
     namespace = "com.example.duper"
     compileSdk {
-        version = release(36)
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "fr.sudotiz.duper"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
